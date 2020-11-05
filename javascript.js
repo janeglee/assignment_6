388045c7f7d1cf2d0307496905ca4f0e10d32485
janeglee <janegolee@gmail.com>
2020-11-04 17:04:12 -0800

    6b

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
 const cart = [];
+let pageInfo = null;
 
+//when click submit button, add roll, amount, glaze to cart
 function handleSubmit (e) {
     e.preventDefault();
     const amount = document.getElementById('amount').value;
     const glaze = document.getElementById('glaze').value;
     const amountGlaze = {
+        roll: pageInfo.pageName,
         amount: amount,
-        glaze: glaze
+        glaze: glaze,
+        subtotal: pageInfo.price * amount,
+        price: pageInfo.price
     }
     cart.push(amountGlaze);
     const jsonCart = JSON.stringify(cart);
@@ -15,9 +20,11 @@ function handleSubmit (e) {
     updateCount();
 }
 
-function onLoad() {
+//whenever page loads, cart will show empty or cart amount
+function onLoad (e) {
+    pageInfo = e;
     const myCart = localStorage.getItem("cartAmount");
-    if(myCart === null) {
+    if(myCart === null || myCart.length === 2) {
       console.log('Cart is empty!');
       return;
     }
@@ -27,9 +34,48 @@ function onLoad() {
         cart.push(amountGlaze);
       });
       updateCount();
+      if(e.pageName == 'Cart') {
+        renderCart()
+      }
     }
 }
 
+//will populate cart page with cart
+function renderCart () {
+    const itemList = document.getElementById('itemList');
+    const totalEl = document.getElementById('total');
+    itemList.innerHTML = '';
+    let total = 0;
+    cart.forEach((item, index) => {
+        const node = document.createElement("li");
+        node.id = index;
+        const stringTemplate = `
+                <ul>
+                    <li>${item.roll}</li>
+                    <li>${item.amount}</li>
+                    <li>${item.glaze}</li>
+                    <li>$ ${item.subtotal}</li>
+                    <li class="removeItem" id="${index}">X</li>
+                </ul>
+        `
+        total+=item.subtotal;
+        node.innerHTML= stringTemplate;
+        itemList.appendChild(node);
+
+        let removeItem = document.getElementById(`${index}`);
+        removeItem.onclick = function (e) {
+            cart.splice(index, 1);
+            const jsonCart = JSON.stringify(cart);
+            localStorage.setItem("cartAmount", jsonCart);
+            updateCount();
+            renderCart();
+        }
+    })
+    totalEl.innerHTML = `<u><b>Total: $ ${total}</b></u>`
+    
+}
+
+//updates the # next to cart on nav bar
 function updateCount (e) {
     const itemCount = document.getElementById('cartItems');
     let totalAmount = 0;
@@ -45,15 +91,17 @@ function handleGlaze () {
     const glaze = document.getElementById('glaze').value;
     const picture = document.getElementById('cinnamon');
     if (glaze == "none") {
-        picture.src="cinnamon.png";
+        picture.src="assets/cinnamon.png";
     }
     else if (glaze =="sugarMilk") {
-        picture.src="sugarMilk.jpg";
+        picture.src="assets/sugarMilk.jpg";
     }
     else if (glaze =="vanillaMilk") {
-        picture.src="vanillaMilk.jpg";
+        picture.src="assets/vanillaMilk.jpg";
     }
     else if (glaze =="doubleChocolate") {
-        picture.src="chocolate.jpg";
+        picture.src="assets/chocolate.jpg";
     }
 }
+
+
