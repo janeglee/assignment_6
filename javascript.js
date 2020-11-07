9547b2b04dbc788d4186497ccafa2eea3387f9b6
janeglee <janegolee@gmail.com>
2020-11-06 18:13:42 -0800

    extra credit

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const cart = [];
+const wishlist = [];
 let pageInfo = null;
 
 //when click submit button, add roll, amount, glaze to cart
@@ -13,17 +14,29 @@ function handleSubmit (e) {
         subtotal: pageInfo.price * amount,
         price: pageInfo.price
     }
-    cart.push(amountGlaze);
-    const jsonCart = JSON.stringify(cart);
-    localStorage.setItem("cartAmount", jsonCart)
-    console.log(cart);
-    updateCount();
+
+    if(e.submitter.name == "submitCart") {
+        cart.push(amountGlaze);
+        const jsonCart = JSON.stringify(cart);
+        localStorage.setItem("cartAmount", jsonCart)
+        console.log(cart);
+        updateCount();
+    }
+
+    else if(e.submitter.name == "submitWish") {
+        wishlist.push(amountGlaze);
+        const jsonWishlist = JSON.stringify(wishlist);
+        localStorage.setItem("wishlistAmount", jsonWishlist)
+        console.log(wishlist);
+    }
+    
 }
 
 //whenever page loads, cart will show empty or cart amount
 function onLoad (e) {
     pageInfo = e;
     const myCart = localStorage.getItem("cartAmount");
+    const myWishlist = localStorage.getItem("wishlistAmount");
     if(myCart === null || myCart.length === 2) {
       console.log('Cart is empty!');
       return;
@@ -33,9 +46,15 @@ function onLoad (e) {
       savedCart.forEach((amountGlaze) => {
         cart.push(amountGlaze);
       });
+
+      const savedWish = JSON.parse(myWishlist);
+      savedWish.forEach((amountGlaze) => {
+          wishlist.push(amountGlaze);
+      })
       updateCount();
       if(e.pageName == 'Cart') {
-        renderCart()
+        renderCart();
+        renderWish();
       }
     }
 }
@@ -55,14 +74,14 @@ function renderCart () {
                     <li>${item.amount}</li>
                     <li>${item.glaze}</li>
                     <li>$ ${item.subtotal}</li>
-                    <li class="removeItem" id="${index}">X</li>
+                    <li class="removeItem" id="cart${index}">X</li>
                 </ul>
         `
         total+=item.subtotal;
         node.innerHTML= stringTemplate;
         itemList.appendChild(node);
 
-        let removeItem = document.getElementById(`${index}`);
+        let removeItem = document.getElementById(`cart${index}`);
         removeItem.onclick = function (e) {
             cart.splice(index, 1);
             const jsonCart = JSON.stringify(cart);
@@ -72,8 +91,38 @@ function renderCart () {
         }
     })
     totalEl.innerHTML = `<u><b>Total: $ ${total}</b></u>`
-    
 }
+
+function renderWish () {
+    const wishlistList = document.getElementById('wishlistList');
+    wishlistList.innerHTML = '';
+    wishlist.forEach((item, index) => {
+        const node2 = document.createElement("li");
+        node2.id = index;
+        const stringTemplate = `
+                <ul>
+                    <li>${item.roll}</li>
+                    <li>${item.amount}</li>
+                    <li>${item.glaze}</li>
+                    <li>$ ${item.subtotal}</li>
+                    <li class="removeItem" id="wishlist${index}">X</li>
+                </ul>
+        `
+        node2.innerHTML= stringTemplate;
+        wishlistList.appendChild(node2);
+
+        let removeItem = document.getElementById(`wishlist${index}`);
+        removeItem.onclick = function (e) {
+            wishlist.splice(index, 1);
+            const jsonWishlist = JSON.stringify(wishlist);
+            localStorage.setItem("wishlistAmount", jsonWishlist);
+            updateCount();
+            renderWish();
+        }
+    })
+}
+
+
 
 //updates the # next to cart on nav bar
 function updateCount (e) {
@@ -105,3 +154,9 @@ function handleGlaze () {
 }
 
 
+const slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
